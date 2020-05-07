8474f65511d1dd12e7780d139ab740a88e1d1093
Ricardo-1998 <00004817@uca.edu.sv>
2020-05-07 10:58:20 -0600

    falta css

---
+++ tareaLabo4/src/test/java/com/uca/capas/tareaLabo4/TareaLabo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.tareaLabo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaLabo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
